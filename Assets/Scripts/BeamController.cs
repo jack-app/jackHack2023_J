2198855c7b8306cd924875d605bb13af1b337243
カエデ <102593414+kaede-hiiragi@users.noreply.github.com>
2023-05-03 14:47:59 +0900

    球がplayerに追従するように修正

--- Assets/Scripts/BeamController.cs
+++ Assets/Scripts/BeamController.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class BeamController : MonoBehaviour
 {
     public GameObject objectToStretch; // 延ばすオブジェクトのGameObject
+    public GameObject Platerobject; // プレイヤーのGameObject
     public float scaleSpeed = 0.5f; // オブジェクトの伸縮速度
     private Vector3 initialScale; // オブジェクトの初期サイズ
     private Vector3 initialPosition; // オブジェクトの初期位置
+    private Vector3 PlayerPosition; // プレイヤーの初期位置
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,20 +21,20 @@ public class BeamController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        PlayerPosition = Platerobject.transform.position;
         if (Input.GetKey(KeyCode.Space)) // スペースキーが押されている間
         {
             if(objectToStretch.transform.localScale.x < 24){
                 Vector3 newScale = new Vector3(objectToStretch.transform.localScale.x + scaleSpeed, objectToStretch.transform.localScale.y, objectToStretch.transform.localScale.z); // オブジェクトの幅を拡大
                 objectToStretch.transform.localScale = newScale; // オブジェクトのサイズを更新
-                float moveDistance = scaleSpeed/2; // オブジェクトを移動する距離
-                Vector3 newPosition = objectToStretch.transform.position + new Vector3(moveDistance, 0, 0); // オブジェクトの新しい位置
-                objectToStretch.transform.position = newPosition; // オブジェクトを移動
             }
+            Vector3 newPosition = PlayerPosition + new Vector3(objectToStretch.transform.localScale.x/2, 0, 0); // オブジェクトの新しい位置
+            objectToStretch.transform.position = newPosition; // オブジェクトを移動
         }
         else // スペースキーが押されていない間
         {
             objectToStretch.transform.localScale = initialScale; // オブジェクトのサイズを初期サイズに戻す
-            objectToStretch.transform.position = initialPosition;// オブジェクトのサイズを初期位置に戻す
+            objectToStretch.transform.position = PlayerPosition;// オブジェクトのサイズを初期位置に戻す
         }
     }
 }
