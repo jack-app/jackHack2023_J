43778decaa4932b5be0458e48c295b46eaf55719
ayu-mushi <ayu.mushi2015@gmail.com>
2023-05-03 16:03:18 +0900

    .

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class BulletController : MonoBehaviour
 {
+    public GameObject target;
+    Vector3 velocity;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,6 @@ public class BulletController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(velocity + (target.transform.position - transform.position)*0.01f);
     }
 }
