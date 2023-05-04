5b747b09e41a09b493e39ee90155d94004f1611a
ayu-mushi <ayu.mushi2015@gmail.com>
2023-05-04 11:38:56 +0900

    ゲームオーバー画面追加

--- Assets/Scripts/FishComponent.cs
+++ Assets/Scripts/FishComponent.cs
@@ -16,16 +16,15 @@ public class FishComponent : MonoBehaviour
     public void Move(Vector3 vec)
     {
         float position_x = Mathf.Max(transform.position.x + vec.x, 0);
-        float position_y = transform.position.y+ vec.y;
+        float position_y = transform.position.y + vec.y;
         transform.position = new Vector3(position_x, position_y, transform.position.z);
     }
 
+    public bool isDead(){
+        return hp <= 0;
+    }
     // Update is called once per frame
     void Update()
     {
-        if(hp <= 0){
-            gameObject.SetActive(false);
-        }
     }
-
 }
