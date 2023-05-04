60580fa0d51bf153b2d6fc41debfa10e0cf95903
カエデ <102593414+kaede-hiiragi@users.noreply.github.com>
2023-05-04 19:05:32 +0900

    Beamをprehub化した

--- Assets/Scripts/BeamController.cs
+++ Assets/Scripts/BeamController.cs
@@ -4,25 +4,26 @@ using UnityEngine;
 
 public class BeamController : MonoBehaviour
 {
-    public GameObject objectToStretch; // 延ばすオブジェクトのGameObject
+    public GameObject NomalBeam; // 延ばすオブジェクトのGameObject
     public GameObject Platerobject; // プレイヤーのGameObject
     public float scaleSpeed = 0.5f; // オブジェクトの伸縮速度
     private Vector3 initialScale; // オブジェクトの初期サイズ
     private Vector3 initialPosition; // オブジェクトの初期位置
     private Vector3 PlayerPosition; // プレイヤーの初期位置
     private bool isExtending = false;  // 延びているかどうかを示すフラグ
+    private GameObject generatedNomal;
 
     public GameObject RomantikBeam;  // 生成するオブジェクトのプレハブ
     public float moveSpeed = 1f;  // 動く速度
 
     private bool isGenerating = false;  // 生成中かどうかを示すフラグ
-    private GameObject generatedObject;  // 生成されたオブジェクトの参照
+    private GameObject generatedRomaitik;  // 生成されたオブジェクトの参照
 
     // Start is called before the first frame update
     void Start()
     {
-        initialScale = objectToStretch.transform.localScale; // 四角形の初期サイズを取得
-        initialPosition = objectToStretch.transform.position; // 四角形の初期位置を取得
+        initialScale = NomalBeam.transform.localScale; // 四角形の初期サイズを取得
+        initialPosition = NomalBeam.transform.position; // 四角形の初期位置を取得
     }
 
     // Update is called once per frame
@@ -32,45 +33,47 @@ public class BeamController : MonoBehaviour
         // スペースキーが押されたら、延びるフラグをtrueにする
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            generatedNomal = Instantiate(NomalBeam, transform.position, Quaternion.identity,Platerobject.transform);
             isExtending = true;
         }
 
         // スペースキーが離されたら、延びるフラグをfalseにする
         if (Input.GetKeyUp(KeyCode.Space))
         {
+            Destroy(generatedNomal);
             isExtending = false;
         }
 
         if (isExtending) // スペースキーが押されている間
         {
-            if(objectToStretch.transform.localScale.x < 150){
-                Vector3 newScale = new Vector3(objectToStretch.transform.localScale.x + scaleSpeed, objectToStretch.transform.localScale.y, objectToStretch.transform.localScale.z); // オブジェクトの幅を拡大
-                objectToStretch.transform.localScale = newScale; // オブジェクトのサイズを更新
+            if(generatedNomal.transform.localScale.x < 150){
+                
+                Vector3 newScale = new Vector3(generatedNomal.transform.localScale.x + scaleSpeed, generatedNomal.transform.localScale.y, generatedNomal.transform.localScale.z); // オブジェクトの幅を拡大
+                generatedNomal.transform.localScale = newScale; // オブジェクトのサイズを更新
             }
-            Vector3 newPosition = PlayerPosition + new Vector3(objectToStretch.transform.localScale.x/2, 0, 0); // オブジェクトの新しい位置
-            objectToStretch.transform.position = newPosition; // オブジェクトを移動
+            Vector3 newPosition = PlayerPosition + new Vector3(generatedNomal.transform.localScale.x/2, 0, 0); // オブジェクトの新しい位置
+            generatedNomal.transform.position = newPosition; // オブジェクトを移動
         }
         else // スペースキーが押されていない間
         {
-            objectToStretch.transform.localScale = initialScale; // オブジェクトのサイズを初期サイズに戻す
-            objectToStretch.transform.position = PlayerPosition;// オブジェクトのサイズを初期位置に戻す
+            
         }
 
 
         if (Input.GetKeyDown(KeyCode.B) && !isGenerating)
         {
-            generatedObject = Instantiate(RomantikBeam, transform.position, Quaternion.identity);
+            generatedRomaitik = Instantiate(RomantikBeam, transform.position, Quaternion.identity,Platerobject.transform);
             isGenerating = true;
         }
 
-        if (generatedObject != null)
+        if (generatedRomaitik != null)
         {
-            Vector3 position = generatedObject.transform.position;
+            Vector3 position = generatedRomaitik.transform.position;
             position.x += moveSpeed * Time.deltaTime;
-            generatedObject.transform.position = position;
-            if(position.x > 8)
+            generatedRomaitik.transform.position = position;
+            if(position.x > 150)
             {
-                Destroy(generatedObject);
+                Destroy(generatedRomaitik);
             }
         }
         else{
