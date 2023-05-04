d408d8d2513a343b3b65b15b7fd5236e0f549f28
ayu-mushi <ayu.mushi2015@gmail.com>
2023-05-04 16:19:05 +0900

    StageClear; Boss

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    [SerializeField]
+    [Tooltip("追跡するオブジェクト")]
     public GameObject target;
+    [SerializeField]
+    [Tooltip("撃つ弾")]
     public GameObject bullet;
     FishComponent fishComponent;
     
@@ -19,22 +23,21 @@ public class EnemyController : MonoBehaviour
     public float speed = 10f;
     public float gunSpeed = 0.8f;
 
-    float timeElapsed;
+    float timeElapsed; // 前回弾を撃ってからの経過時間
     float timeout = 2.0f;
     void Update()
     {
         timeElapsed += Time.deltaTime;
         Vector3 r = new Vector3 (Random.Range(-0.1f, 0.1f), Random.Range(-0.1f, 0.1f));
         fishComponent.Move((target.transform.position - transform.position).normalized * speed * Time.deltaTime); // 索敵
-
     
-        if(timeElapsed > timeout){
+        if(timeElapsed > timeout){ // 一定時間おきに弾を撃つ
             GameObject bullet_instance = Instantiate(bullet);
             bullet_instance.transform.parent = gameObject.transform;
             bullet_instance.transform.localPosition = new Vector3 (0,0,0);
             timeElapsed = 0.0f;
-            BulletController bc= bullet_instance.GetComponent<BulletController>();
-            bc.velocity = (target.transform.position - transform.position).normalized * gunSpeed;
+            BulletController bc = bullet_instance.GetComponent<BulletController>();
+            bc.velocity = (target.transform.position - transform.position).normalized * gunSpeed; // 弾のスピード
         }
     }
 
--- Assets/Scripts/FishComponent.cs
+++ Assets/Scripts/FishComponent.cs
@@ -6,21 +6,26 @@ public class FishComponent : MonoBehaviour
 {
     // Start is called before the first frame update
 
+    [SerializeField]
+    [Tooltip("HP")]
     public int hp;
+
+    [SerializeField]
+    [Tooltip("最大HP")]
     public int max_hp;
     void Start()
     {
-        hp = max_hp;
+        hp = max_hp; // 最初のHPは最大値
     }
 
     public void Move(Vector3 vec)
     {
-        float position_x = Mathf.Max(transform.position.x + vec.x, 0);
+        float position_x = Mathf.Max(transform.position.x + vec.x, 0); // x座標が0より左には行かない
         float position_y = transform.position.y + vec.y;
         transform.position = new Vector3(position_x, position_y, transform.position.z);
     }
 
-    public bool isDead(){
+    public bool isDead(){ // 死亡しているかを表す
         return hp <= 0;
     }
     // Update is called once per frame
